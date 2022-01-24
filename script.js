54f091e68cc43a9836c1eae6c1b2fe9900a47435
syrgo <syrga.v30@gmail.com>
2022-01-24 22:35:38 +0300

    researching javascript

---
+++ script.js
@@ -0,0 +1,65 @@
+// FINDING REMAINDER
+var remainder;
+remainder = 11 % 3;
+// console.log(remainder);
+
+
+// AUGUMENTED MATH OPERATIONS
+var a = 3;
+var b = 17;
+var c = 12;
+
+a += 12; //short plus equals
+b -= 9; //short minus equals
+c /= 6; //short times equals
+c *= 9; //short divided by equal
+// console.log(a, b, c);
+
+
+// DECLARE STRING VARIABLES
+var myFirstName = "Celine";
+var myLastName = "Han";
+
+
+// ESCAPING LITERAL QUOTES
+var myStr = "I am a \"double quoted\" string inside \"double quotes\"";
+// console.log(myStr);
+var myStr = '<a href="http://www.example.com" target="_blank">Link</a>'; //QUOTING STRINGS WITH SINGLE QUOTES
+// console.log(myStr);
+
+
+// ESCAPE SEQUENCES
+/* 
+CODE OUTPUT
+\'   single quote
+\"   double quote
+\\   backslash
+\n   newline
+\r   carriage return
+\t   tab
+\b   backspace
+\f   form feed
+*/
+var myStr = "FirstLine\n\t\SecondLine\nThirdLine";
+// console.log(myStr);
+
+
+// PLUS OPERATOR
+var ourStr = "I come first. " + "\nI come second.";
+// console.log(ourStr);
+var myStr = "This is the start. " + "This is the middle.";
+// console.log(myStr);
+ourStr += "\nI come third.";
+// console.log(ourStr);
+myStr += "\tThis is the end.";
+// console.log(myStr);
+
+
+// CONSTRUCTING STRINGS WITH VARIABLES
+var ourName = "IT Academy Karakol";
+// var newStr = prompt("Hello, our name is " + ourName + " , how are you?");
+
+var myName = "Celine Han";
+var myString = "My name is " + myName + " and I am cool!";
+// console.log(myString);
+
